334c2ed2ab90e5e67a92ff7646561f9c50acb005
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2015-07-29 15:48:35 -0400

    OSC changes

--- Sky Creatures_Unity/Assets/Scripts/Communication/OSCPureDataConnection.cs
+++ Sky Creatures_Unity/Assets/Scripts/Communication/OSCPureDataConnection.cs
@@ -5,47 +5,63 @@ using UnityEngine;
 using System.Collections;
 using UnityOSC;
 
-public class OSCPureDataConnection : MonoBehaviour
+public class OSCPureDataConnection : SingletonMonoBehaviour<OSCPureDataConnection>
 {
     private const string ClientName = "PD-out";
+    private const string ClientName2 = "PD-out2";
 
     [SerializeField] private string clientTargetAddress = "127.0.0.1";
-    [SerializeField] private int clientTargetIP = 9001;
+    [SerializeField] private int clientTargetPort = 9001;
+    [SerializeField] private int clientTargetPort2 = 9002;
 
     private void Awake()
     {
-        OSCHandler.Instance.CreateClient(ClientName, IPAddress.Parse(clientTargetAddress), clientTargetIP);
-    }
-
-    private void Update()
-    {
-        // Type something and it shows up in the PureData console
-        if (Input.anyKeyDown)
-        {
-            OSCHandler.Instance.SendMessageToClient(ClientName, "/just/some/path", Input.inputString);
-        }
+        OSCHandler.Instance.CreateClient(ClientName, IPAddress.Parse(clientTargetAddress), clientTargetPort);
+        OSCHandler.Instance.CreateClient(ClientName2, IPAddress.Parse(clientTargetAddress), clientTargetPort2);
     }
 
     public void SendDirections(float up, float down, float left, float right)
     {
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/up", up);
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/down", down);
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/left", left);
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/right", right);
+        Send("up", up);
+        Send("down", down);
+        Send("left", left);
+        Send("right", right);
     }
 
     public void SendLayer(int layer)
     {
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/layer", layer);
+        Send("layer", layer);
     }
 
     public void SendCreatureCatch()
     {
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/creaturecatch", 1);
+        Send("creaturecatch", 1);
     }
 
     public void SendCreatureProximity(float proximity)
     {
-        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/creatureproximity", proximity);
+        Send("creatureproximity", proximity);
+    }
+
+    public void SendCreatureType(Creature creature)
+    {
+        /* 0 = giraffe
+           1 = goat
+           2 = dog
+           3 = monkey
+           4 = cat
+           5 = bird2 */
+        Send("creaturetype", creature.PureDataCreatureIndex);
+    }
+
+    public void SendEndGame()
+    {
+        Send("endgame", 1);
+    }
+
+    public void Send<T>(string pathEnd, T value)
+    {
+        OSCHandler.Instance.SendMessageToClient(ClientName, "/toby/" + pathEnd, value);
+        OSCHandler.Instance.SendMessageToClient(ClientName2, "/toby/" + pathEnd, value);
     }
 }
